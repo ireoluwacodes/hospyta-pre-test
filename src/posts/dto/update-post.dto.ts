527ec11d58da3a91e4b56e898c17dffa65059e3a
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-07-27 21:38:46 +0100

    fix

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
-import { AtLeastOneField } from './custom-dto-decorator.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @AtLeastOneField({
-    message: 'At least one field must be provided',
-  })
-  static readonly dummyField: any;
-}
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
