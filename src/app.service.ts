05893d34ce20041d1d18811770379f5eac189bcc
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-07-27 20:26:21 +0100

    docs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHome(): string {
-    return 'Documentation can be found @ <a href="https://documenter.getpostman.com/view/22684334/2s9YsMAX2Y">docs</a>';
+    return 'Documentation can be found @ <a href="https://documenter.getpostman.com/view/22684334/2sA3kaBJrw">docs</a>';
   }
 }
